e22f70f9e01c3f05af87420f7e51f636eb13fd75
IchBinAyhaan <165468545+IchBinAyhaan@users.noreply.github.com>
2024-08-04 13:46:02 +0400

    Add files via upload

---
+++ Task2 (1).sql
@@ -0,0 +1,88 @@
+CREATE DATABASE Course2
+
+USE Course2
+
+CREATE TABLE Groups (
+    Id INT PRIMARY KEY,
+    Name NVARCHAR(50) NOT NULL,
+    Limit INT NOT NULL,
+    BeginDate DATE NOT NULL,
+    EndDate DATE
+);
+
+CREATE TABLE Students (
+    Id INT PRIMARY KEY,
+    Name NVARCHAR(50) NOT NULL,
+    Surname NVARCHAR(50) NOT NULL, 
+    Email NVARCHAR(100),
+    PhoneNumber NVARCHAR(20),
+    BirthDate DATE,
+    GPA DECIMAL(3, 2) 
+);
+INSERT INTO Students (Id, Name, Surname, Email, PhoneNumber, BirthDate, GPA)
+VALUES
+    (1, 'John', 'Doe', 'john.doe@example.com', '123456789', '2000-01-01', 3.75),
+    (2, 'Jane', 'Smith', 'jane.smith@example.com', '987654321', '1999-08-15', 3.90),
+    (3, 'Alice', 'Johnson', 'alice.johnson@example.com', '456789123', '2002-03-20', 3.60);
+
+
+
+CREATE TRIGGER CheckGroupLimit
+ON Students
+FOR INSERT
+AS
+BEGIN
+    DECLARE @GroupId INT;
+    DECLARE @GroupLimit INT;
+    DECLARE @CurrentStudentCount INT;
+
+    SELECT @GroupId = g.Id, @GroupLimit = g.Limit
+    FROM Groups g
+    JOIN inserted i ON i.GroupId = g.Id;
+
+    SELECT @CurrentStudentCount = COUNT(*)
+    FROM Students
+    WHERE GroupId = @GroupId;
+
+    IF @CurrentStudentCount >= @GroupLimit
+    BEGIN
+        RAISERROR('Qrup limiti kecildi', 16, 1);
+        ROLLBACK TRANSACTION; 
+    END
+END;
+
+
+CREATE TRIGGER CheckStudentAge
+ON Students
+FOR INSERT
+AS
+BEGIN
+    DECLARE @StudentBirthDate DATE;
+    DECLARE @StudentId INT;
+
+    SELECT @StudentBirthDate = i.BirthDate, @StudentId = i.Id
+    FROM inserted i;
+
+    DECLARE @StudentAge INT;
+    SET @StudentAge = DATEDIFF(YEAR, @StudentBirthDate, GETDATE());
+
+    IF @StudentAge < 16
+    BEGIN
+        RAISERROR('Yas 16-dan boyuk olmalidir', 16, 1);
+        DELETE FROM Students WHERE Id = @StudentId;
+    END
+END;
+
+
+CREATE FUNCTION GetGroupAverageGPA (@groupId INT)
+RETURNS DECIMAL(3, 2)
+AS
+BEGIN
+    DECLARE @AvgGPA DECIMAL(3, 2);
+
+    SELECT @AvgGPA = AVG(GPA)
+    FROM Students
+    WHERE GroupId = @groupId;
+
+    RETURN @AvgGPA;
+END;
---
+++ Task2 (2).sql
@@ -0,0 +1,97 @@
+	CREATE DATABASE Work2
+
+	USE Work2
+
+	CREATE TABLE Departments (
+    Id INT PRIMARY KEY,
+    Name NVARCHAR(20) NOT NULL
+);
+
+CREATE TABLE Positions (
+    Id INT PRIMARY KEY,
+    Name NVARCHAR(20) NOT NULL,
+    Limit INT NOT NULL
+);
+
+CREATE TABLE Workers (
+    Id INT PRIMARY KEY,
+    Name NVARCHAR(30) NOT NULL,
+    Surname NVARCHAR(30) NOT NULL,
+    PhoneNumber NVARCHAR(20),
+    Salary DECIMAL(10, 2),
+    BirthDate DATE
+);
+INSERT INTO Departments (Id, Name)
+VALUES (1, 'IT'),
+       (2, 'HR'),
+       (3, 'Finance');
+
+INSERT INTO Positions (Id, Name, Limit)
+VALUES (1, 'Software Developer', 10),
+       (2, 'HR Manager', 5),
+       (3, 'Financial Analyst', 8);
+
+INSERT INTO Workers (Id, Name, Surname, PhoneNumber, Salary, BirthDate)
+VALUES (1, 'John', 'Doe', '123456789', 5000.00, '1990-05-15'),
+       (2, 'Jane', 'Smith', '987654321', 4500.00, '1995-10-20'),
+       (3, 'Alice', 'Johnson', '456789123', 4800.00, '1988-03-25');
+
+
+CREATE FUNCTION GetAverageSalaryByDepartment (@departmentId INT)
+RETURNS DECIMAL(10, 2)
+AS
+BEGIN
+    DECLARE @AvgSalary DECIMAL(10, 2);
+
+    SELECT @AvgSalary = AVG(Salary)
+    FROM Workers w
+    JOIN Positions p ON w.PositionId = p.Id
+    WHERE p.DepartmentId = @departmentId;
+
+    RETURN @AvgSalary;
+END;
+
+CREATE TRIGGER CheckWorkerAge
+ON Workers
+FOR INSERT
+AS
+BEGIN
+    DECLARE @WorkerBirthDate DATE;
+    DECLARE @WorkerId INT;
+
+    SELECT @WorkerBirthDate = i.BirthDate, @WorkerId = i.Id
+    FROM inserted i;
+
+    DECLARE @WorkerAge INT;
+    SET @WorkerAge = DATEDIFF(YEAR, @WorkerBirthDate, GETDATE());
+
+    IF @WorkerAge < 18
+    BEGIN
+        RAISERROR('Worker 18 yasindan boyuk olmalidir ', 16, 1);
+        DELETE FROM Workers WHERE Id = @WorkerId; 
+    END
+END;
+
+CREATE TRIGGER CheckPositionLimit
+ON Workers
+FOR INSERT
+AS
+BEGIN
+    DECLARE @PositionId INT;
+    DECLARE @PositionLimit INT;
+    DECLARE @CurrentWorkersCount INT;
+
+    SELECT @PositionId = w.PositionId, @PositionLimit = p.Limit
+    FROM inserted w
+    JOIN Positions p ON w.PositionId = p.Id;
+
+    SELECT @CurrentWorkersCount = COUNT(*)
+    FROM Workers
+    WHERE PositionId = @PositionId;
+
+    IF @CurrentWorkersCount >= @PositionLimit
+    BEGIN
+        RAISERROR('Position limiti kecib', 16, 1);
+        ROLLBACK TRANSACTION; 
+    END
+END;
